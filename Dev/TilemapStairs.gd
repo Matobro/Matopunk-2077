2d0aee47c249d993fc494ef2dc977b59e90cedd8
Matobro <Harri.taberman@hotmail.com>
2026-01-15 16:13:37 +0200

    stairs fixes

--- Dev/TilemapStairs.gd
+++ Dev/TilemapStairs.gd
@@ -1,7 +1,8 @@
 extends TileMapLayer
 
 @export var stair_area: PackedScene
-@export var stair_solid_tile_coords: Array[Vector2i]
+@export var stair_left_tiles: Array[Vector2i]
+@export var stair_right_tiles: Array[Vector2i]
 @export var is_entrance: bool
 
 func _ready() -> void:
@@ -9,12 +10,24 @@ func _ready() -> void:
 
 
 func setup_stairs_area():
+	var stair_tiles: Array[Vector2i] = stair_left_tiles + stair_right_tiles
 	for cell in get_used_cells():
-		if get_cell_atlas_coords(cell) in stair_solid_tile_coords:
+		var tile_coords = get_cell_atlas_coords(cell)
+		if tile_coords in stair_tiles:
 			var area = stair_area.instantiate()
 			var col = area.get_child(0)
 			area.position = map_to_local(cell)
-			if is_entrance:
+			if is_entrance and tile_coords in stair_left_tiles:
 				col.rotation = 0.0
 				area.position.y += 16
+				area.position.x -= 30
+
+			elif is_entrance and tile_coords in stair_right_tiles:
+				col.rotation = 0.0
+				area.position.y += 16
+				area.position.x -= 5
+
+			elif !is_entrance and tile_coords in stair_right_tiles:
+				area.rotation = deg_to_rad(270)
+
 			add_child(area)
